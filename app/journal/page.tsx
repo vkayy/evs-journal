947db9e0dcad65f440597252f27297b12bf5ece9
vkayy <vkrice2475@gmail.com>
2023-09-24 05:27:23 +0100

    feat: add wip text to journal

--- app/journal/page.tsx
+++ app/journal/page.tsx
@@ -1,7 +1,21 @@
+import MainSection from "@/components/MainSection";
+
 export default function Page() {
   return (
     <>
-      <main className="main">
+      <main className="main main_journal">
+        <MainSection
+          id="journal"
+          heading="hey there!"
+          content={
+            <>
+              <h1 className="text-center text_heading mb-8">
+                THIS PAGE IS A WORK IN PROGRESS
+              </h1>
+              <p className="text-center text_paragraph">come back soon!</p>
+            </>
+          }
+        ></MainSection>
       </main>
     </>
   );
