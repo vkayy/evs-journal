730a6c76dc9fab474cd650b20dfa0b037525732e
vkayy <vkrice2475@gmail.com>
2023-09-26 16:57:37 +0100

    refactor: use children prop instead of content prop

--- components/MainSection.tsx
+++ components/MainSection.tsx
@@ -1,16 +1,16 @@
 interface MainProps {
   id: string;
   heading: string;
-  content?: React.ReactNode;
+  children: React.ReactNode;
 }
 
-function MainSection({ id, heading, content }: MainProps) {
+function MainSection({ id, heading, children }: MainProps) {
   return (
     <section className="general-section" id={id}>
       <h2 className="general-section__text_heading text text_heading text_shadow general-section__entry-animation general-section__entry-animation_top">
         {heading}
       </h2>
-      <div className="general-section__content general-section__entry-animation general-section__entry-animation_bottom">{content}</div>
+      <div className="general-section__content general-section__entry-animation general-section__entry-animation_bottom">{children}</div>
     </section>
   );
 }
